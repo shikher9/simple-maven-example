44da3ddc587b2a943122add44a9f20cb11cb320b
Shikher Pandey <shikher9@gmail.com>
2016-08-31 16:55:55 +0530

    Added tests

---
+++ MavenExample/src/main/java/com/example/maven/App.java
@@ -0,0 +1,11 @@
+package com.example.maven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+	public static void main( String[] args ) {
+		System.out.println( "Hello Simple Maven Example" );
+	}
+}
